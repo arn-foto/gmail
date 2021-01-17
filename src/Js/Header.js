aca080eccdcfbd50f76893e98db3771971200573
Anthony N <foto.arn@gmail.com>
2021-01-17 00:21:30 -0800

    + searchbar to header

--- src/Js/Header.js
+++ src/Js/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
-import { IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import logo from "../img/gmail-logo.png";
 
 function Header() {
 	return (
@@ -23,7 +24,15 @@ function Header() {
 				<input placeholder="Sarch mail" type="text" />
 				<ArrowDropDownIcon className="Header__inputCaret" />
 			</div>
-			<div className="header__right"></div>
+			<div className="header__right">
+				<IconButton>
+					<AppsIcon />
+				</IconButton>
+				<IconButton>
+					<NotificationsIcon />
+				</IconButton>
+				<Avatar />
+			</div>
 		</div>
 	);
 }
