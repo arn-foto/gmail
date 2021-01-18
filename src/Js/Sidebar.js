98eefc9bca8637b63d7d8d95ba24bc8dbecaab4d
Anthony N <foto.arn@gmail.com>
2021-01-18 12:44:40 -0800

    + adding icons to sidebar

--- src/Js/Sidebar.js
+++ src/Js/Sidebar.js
@@ -1,10 +1,15 @@
-import { Button } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
-import StarIcon from "@material-ui/icons/Star";
-import InboxIcon from "@material-ui/icons/Inbox";
 import React from "react";
 import "../styles/Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import { Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import NoteIcon from "@material-ui/icons/Note";
+import StarIcon from "@material-ui/icons/Star";
+import InboxIcon from "@material-ui/icons/Inbox";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 
 function Sidebar() {
 	return (
@@ -22,6 +27,11 @@ function Sidebar() {
 				selected={true}
 			/>
 			<SidebarOption Icon={StarIcon} title="Starred" number={87} />
+			<SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={87} />
+			<SidebarOption Icon={LabelImportantIcon} title="Important" number={87} />
+			<SidebarOption Icon={NearMeIcon} title="Sent" number={87} />
+			<SidebarOption Icon={NoteIcon} title="Drafts" number={87} />
+			<SidebarOption Icon={ExpandMoreIcon} title="More" number={87} />
 		</div>
 	);
 }
