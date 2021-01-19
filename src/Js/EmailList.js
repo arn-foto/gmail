6113fb445a8b603943b41b21eaa1441829b8e9b9
Anthony N <foto.arn@gmail.com>
2021-01-18 17:43:10 -0800

    + Adding Email & EmailList icons

--- src/Js/EmailList.js
+++ src/Js/EmailList.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "../styles/EmailList.css";
+import { Checkbox, IconButton } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function EmailList() {
-	return <div className="emaiLList"></div>;
+	return (
+		<div className="emaiLList">
+			<div className="emaillist__settings">
+				<div className="emaillist__settingleft">
+					<Checkbox />
+					<IconButton>
+						<ArrowDropDownIcon />
+					</IconButton>
+					<IconButton>
+						<RedoIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVertIcon />
+					</IconButton>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default EmailList;
