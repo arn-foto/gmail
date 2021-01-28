d24d755b6859e4d7aa94b581315cc1207d3d0b9c
Anthony N <foto.arn@gmail.com>
2021-01-27 22:39:42 -0800

    + Styling Login component

--- src/Js/Login.js
+++ src/Js/Login.js
@@ -1,14 +1,38 @@
+import { Button } from "@material-ui/core";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
 import "../styles/Login.css";
+import { auth, provider } from "./firebase";
 
 function Login() {
+	const dispatch = useDispatch();
+
+	const signIn = () => {
+		auth
+			.signInWithPopup(provider)
+			.then(({ user }) => {
+				dispatch(
+					login({
+						displayname: user.displayName,
+						email: user.email,
+						photoUrl: user.photoURL,
+					})
+				);
+			})
+			.catch((error) => alert(error.message));
+	};
+
 	return (
 		<div className="login">
 			<div className="login__container">
 				<img
 					src="https://static.dezeen.com/uploads/2020/10/gmail-google-logo-rebrand-workspace-design_dezeen_2364_col_0.jpg"
 					alt="Gmail-Clone"
-				></img>
+				/>
+				<Button variant="contained" color="primary" onclick={signIn}>
+					Login
+				</Button>
 			</div>
 		</div>
 	);
