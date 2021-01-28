fe4b5e82a4560338bfe902619c0d9a8ad43057a9
Anthony N <foto.arn@gmail.com>
2021-01-27 22:16:59 -0800

    + Create Login component

--- src/Js/App.js
+++ src/Js/App.js
@@ -1,35 +1,42 @@
+import "../styles/App.css";
 import React from "react";
+import Login from "./Login";
 import Mail from "../Js/Mail";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import SendMail from "./SendMail";
 import EmailList from "../Js/EmailList";
-import "../styles/App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 import { selectSendMessageIsOpen } from "../features/mailSlice";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+	const user = useSelector(selectUser);
 
 	return (
 		<Router>
-			<div className="app">
-				<Header />
-				<div className="app__body">
-					<Sidebar />
+			{!user ? (
+				<Login />
+			) : (
+				<div className="app">
+					<Header />
+					<div className="app__body">
+						<Sidebar />
 
-					<Switch>
-						<Route path="/mail">
-							<Mail />
-						</Route>
-						<Route path="/">
-							<EmailList />
-						</Route>
-					</Switch>
+						<Switch>
+							<Route path="/mail">
+								<Mail />
+							</Route>
+							<Route path="/">
+								<EmailList />
+							</Route>
+						</Switch>
+					</div>
+					{sendMessageIsOpen && <SendMail />}
 				</div>
-				{sendMessageIsOpen && <SendMail />}
-			</div>
+			)}
 		</Router>
 	);
 }
